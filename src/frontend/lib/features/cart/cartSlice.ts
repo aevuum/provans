f246b0cb4d8892a247fda68300a62e801bd77257
aevuum <a.polyuga@millida.org>
2025-09-02 00:08:21 +0400

    fix

--- src/frontend/lib/features/cart/cartSlice.ts
+++ src/frontend/lib/features/cart/cartSlice.ts
@@ -1,6 +1,6 @@
 // lib/features/cart/cartSlice.ts
 import { createSlice, PayloadAction } from '@reduxjs/toolkit';
-import { Product } from '../../../types';
+import { Product } from '../../../types/index';
 
 interface CartItem extends Product {
   count: number;
